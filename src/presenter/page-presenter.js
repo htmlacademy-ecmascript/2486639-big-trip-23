1bc79172d4a96511df24215f390c4d126169d692
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-04-26 20:51:41 +0700

    докоммит

--- src/presenter/page-presenter.js
+++ src/presenter/page-presenter.js
@@ -7,28 +7,32 @@ import FiltersView from '../view/filters-view.js';
 import NewEventButtonView from '../view/new-event-button-view.js';
 //
 import ContentView from '../view/content-view.js';
-
-const pageBodyContainerElement = document.querySelector('.page-body');
+//
 const headerMainSelector = 'div.trip-main';
 
 export default class PagePresenter {
   constructor() {
+    //
     this.headerComponent = new HeaderView();
     this.headerElement = this.headerComponent.getElement();
+    //!! this.headerMainSectionElement понадобится в других методах?
     this.headerMainSectionElement = this.headerElement.querySelector(headerMainSelector);
+    //
     this.contentComponent = new ContentView();
+    //!! this.contentElement понадобится в других методах?
     this.contentElement = this.contentComponent.getElement();
   }
 
   init() {
-    pageBodyContainerElement.innerHTML = '';
+    const bodyElement = document.body;
+    bodyElement.innerHTML = '';
 
     render(new InfoView(), this.headerMainSectionElement);
     render(new FiltersView(), this.headerMainSectionElement);
     render(new NewEventButtonView(), this.headerMainSectionElement);
 
-    render(this.headerComponent, pageBodyContainerElement);
-    render(this.contentComponent, pageBodyContainerElement);
+    render(this.headerComponent, bodyElement);
+    render(this.contentComponent, bodyElement);
 
     //for (let i = 0; i < 3; i++) {
     //  render(new TaskView(), this.taskListComponent.getElement());
--- src/view/header-view.js
+++ src/view/header-view.js
@@ -1,12 +1,14 @@
 import { createElement } from '../render.js';
 
+//!! до какого уровня разбивать?
+//!! лого будет меняться в зависимости от окончательного маршрута?
 const createHeaderTemplate = () => `<header class="page-header">
-<div class="page-body__container  page-header__container">
-  <img class="page-header__logo" src="img/logo.png" width="42" height="42" alt="Trip logo">
+  <div class="page-body__container  page-header__container">
+    <img class="page-header__logo" src="img/logo.png" width="42" height="42" alt="Trip logo">
 
-  <div class="trip-main">
+    <div class="trip-main">
+    </div>
   </div>
-</div>
 </header>`;
 
 export default class HeadView {
