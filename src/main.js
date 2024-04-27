bdf595ffbbb30b1a9e23245c9c04cdb62baf8f21
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-04-27 10:22:50 +0700

    content-presenter

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import HeaderPresenter from './presenter/header-presenter.js';
 import ContentPresenter from './presenter/content-presenter.js';
 
 const bodyElement = document.body;
+//
 const headerContainerElement = bodyElement.querySelector('div.page-body__container.page-header__container');
 const headerMainElement = headerContainerElement.querySelector('div.trip-main');
 const headerFiltersElement = headerContainerElement.querySelector('div.trip-controls__filters');
+//
+const eventsSectionElement = bodyElement.querySelector('section.trip-events');
 
+//
 const headerPresenter = new HeaderPresenter();
 const contentPresenter = new ContentPresenter();
 
 headerPresenter.init({ mainElement: headerMainElement, filtersElement: headerFiltersElement });
-contentPresenter.init({ containerElement: bodyElement });
+contentPresenter.init({ containerElement: bodyElement, tripEventsSectionElement: eventsSectionElement });
--- src/presenter/content-presenter.js
+++ src/presenter/content-presenter.js
@@ -8,27 +8,23 @@ import EventItemView from '../view/event-item-view.js';
 import EventFormView from '../view/event-form-view.js';
 import MessageView from '../view/message-view.js';
 
-const tripEventsSectionSelector = 'section.trip-events';
 const EVENT_ITEM_COUNT = 3;
 
 export default class PagePresenter {
   constructor() {
-
     this.contentComponent = new ContentView();
-    this.contentElement = this.contentComponent.getElement();
-    this.tripEventsSectionElement = this.contentElement.querySelector(tripEventsSectionSelector);
     this.eventsListComponent = new EventsListView();
     this.eventsListElement = this.eventsListComponent.getElement();
   }
 
-  init({ containerElement }) {
-    render(new SortingView(), this.tripEventsSectionElement);
+  init({ containerElement, tripEventsSectionElement }) {
+    render(new SortingView(), tripEventsSectionElement);
     render(new EventFormView(), this.eventsListElement);
     for (let i = 0; i < EVENT_ITEM_COUNT; i++) {
       render(new EventItemView(), this.eventsListElement);
     }
-    render(this.eventsListComponent, this.tripEventsSectionElement);
-    render(new MessageView(), this.tripEventsSectionElement);
+    render(this.eventsListComponent, tripEventsSectionElement);
+    render(new MessageView(), tripEventsSectionElement);
     render(this.contentComponent, containerElement);
   }
 }
