1bed8ba889e2ba99686c985660f8639fb5586e01
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-05-07 17:03:48 +0700

    изменения в презенторах

--- src/presenter/content-presenter.js
+++ src/presenter/content-presenter.js
@@ -6,37 +6,38 @@ import MessageView from '../view/message-view.js';
 import { EVENT_TYPES } from '../const.js';
 
 export default class ContentPresenter {
-  eventsListComponent = new EventsListView();
+  #containerElement = null;
+  #eventsModel = null;
+
+  #eventsListComponent = new EventsListView();
 
   constructor({ containerElement, eventsModel }) {
-    this.containerElement = containerElement;
-    this.eventsModel = eventsModel;
+    this.#containerElement = containerElement;
+    this.#eventsModel = eventsModel;
   }
 
   init() {
-    const eventsListElement = this.eventsListComponent.element;
-    const { eventsModel } = this;
-
-    const { events, destinationNames } = eventsModel;
+    const eventsListElement = this.#eventsListComponent.element;
+    const { events, destinationNames } = this.#eventsModel;
 
     this.events = [...events]; //! временно. сохранить то что будет диспользоваться в других методах.
 
     //! временно выводим форму редактирования
     const event = events[0];
-    const destination = eventsModel.getDestinationById(event.destination); //? где же правильней собирать данные? может во View?
-    const offers = eventsModel.getAvailableEventOffers(event);
+    const destination = this.#eventsModel.getDestinationById(event.destination); //? где же правильней собирать данные? может во View?
+    const offers = this.#eventsModel.getAvailableEventOffers(event);
     render(new EventFormView(event, EVENT_TYPES, destinationNames, destination, offers), eventsListElement);
     //! временно выводим несколько событий
     for (let i = 1; i < events.length; i++) {
       const currentEvent = events[i];
-      const { name: destinationName } = eventsModel.getDestinationById(currentEvent.destination);
-      const eventOffers = eventsModel.getEventOffers(currentEvent);
+      const { name: destinationName } = this.#eventsModel.getDestinationById(currentEvent.destination);
+      const eventOffers = this.#eventsModel.getEventOffers(currentEvent);
       render(new EventItemView(currentEvent, destinationName, eventOffers), eventsListElement);
     }
 
     //! с пустым списком нужно убрать весь блок и вывести сообщение! должен подойти MessageView <p class="trip-events__msg">Click New Event to create your first point</p>
-    render(this.eventsListComponent, this.containerElement);
+    render(this.#eventsListComponent, this.#containerElement);
 
-    render(new MessageView(), this.containerElement); //! временно
+    render(new MessageView(), this.#containerElement); //! временно
   }
 }
--- src/presenter/info-presenter.js
+++ src/presenter/info-presenter.js
@@ -2,14 +2,17 @@ import { render, RenderPosition } from '../framework/render.js';
 import InfoView from '../view/info-view.js';
 
 export default class InfoPresenter {
+  #containerElement = null;
+  #eventsModel = null;
+
   constructor({ containerElement, eventsModel }) {
-    this.containerElement = containerElement;
-    this.eventsModel = eventsModel;
+    this.#containerElement = containerElement;
+    this.#eventsModel = eventsModel;
   }
 
   init() {
-    const info = this.eventsModel.info; //! временно
+    const info = this.#eventsModel.info; //! временно
 
-    render(new InfoView(info), this.containerElement, RenderPosition.AFTERBEGIN);
+    render(new InfoView(info), this.#containerElement, RenderPosition.AFTERBEGIN);
   }
 }
