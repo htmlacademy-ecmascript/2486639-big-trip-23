7d49403a3540d87ccde4de66c1c45b9bb26cdf48
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-05-13 18:08:36 +0700

    наработки

--- src/presenter/trip-events-presenter.js
+++ src/presenter/trip-events-presenter.js
@@ -17,6 +17,9 @@ export default class TripEventsPresenter {
 
   #eventsListComponent = new EventsListView();
 
+  #hiddenEventItemComponent = null;
+  #openedEventFormComponent = null;
+
   constructor({ containerElement, tripEventsModel }) {
     this.#containerElement = containerElement;
     this.#tripEventsModel = tripEventsModel;
@@ -41,13 +44,8 @@ export default class TripEventsPresenter {
   }
 
   #renderEventItem(event, eventsListElement) {
-    //? по ТЗ нужен Enter?
-    const onEscKeyDown = (evt) => {
-      if (isEscapeKey(evt)) {
-        evt.preventDefault();
-        replaceFormToItem();
-      }
-    };
+    let eventItemComponent = null;
+    let eventFormComponent = null;
 
     // Подготовим недостющие данные для отображения события в списке и при редактировании
     const { destination, type, offers } = event;
@@ -63,11 +61,11 @@ export default class TripEventsPresenter {
       eventOffers,
       onFavoriteClick: null, //! временно
       onEditClick: () => {
-        replaceItemToForm();
+        this.replaceItemToForm(eventFormComponent, eventItemComponent);
       }
     };
 
-    const eventItemComponent = new EventItemView(eventItem);
+    eventItemComponent = new EventItemView(eventItem);
 
     //! название, может и не тужно сразу передать в функцию!
     const eventEdit = {
@@ -77,26 +75,47 @@ export default class TripEventsPresenter {
       destinations: this.#destinations,
       onSubmit: () => {
         //! сохранить изменения
-        replaceFormToItem();
+        this.replaceFormToItem(eventItemComponent, eventFormComponent);
       },
       onClose: () => {
-        replaceFormToItem();
+        //console.log(this);
+        this.replaceFormToItem(eventItemComponent, eventFormComponent);
       }
     };
 
-    const eventFormComponent = new EventFormView(eventEdit);
-
-    function replaceItemToForm() {
-      replace(eventFormComponent, eventItemComponent);
-      document.addEventListener('keydown', onEscKeyDown);
-      //? тут прокрутить страницу если форма отрисовалась ниже видимой области?
-    }
-
-    function replaceFormToItem() {
-      replace(eventItemComponent, eventFormComponent);
-      document.removeEventListener('keydown', onEscKeyDown);
-    }
+    eventFormComponent = new EventFormView(eventEdit);
 
     render(eventItemComponent, eventsListElement);
   }
+
+  //? по ТЗ нужен Enter?
+  onDocumentEscKeyDown = (evt) => {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      if (this.hideItem && this.showForm) {
+        this.replaceFormToItem(this.hideItem, this.showForm);
+      }
+    }
+  };
+
+  replaceItemToForm = (eventFormComponent, eventItemComponent) => {
+    if (this.hideItem && this.showForm) {
+      this.replaceFormToItem(this.hideItem, this.showForm);
+    }
+
+    this.hideItem = eventItemComponent;
+    this.showForm = eventFormComponent;
+
+    replace(eventFormComponent, eventItemComponent);
+    document.addEventListener('keydown', this.onDocumentEscKeyDown);
+    //? тут прокрутить страницу если форма отрисовалась ниже видимой области?
+  };
+
+  replaceFormToItem = (eventItemComponent, eventFormComponent) => {
+    this.hideItem = null;
+    this.showForm = null;
+
+    replace(eventItemComponent, eventFormComponent);
+    document.removeEventListener('keydown', this.onDocumentEscKeyDown);
+  };
 }
