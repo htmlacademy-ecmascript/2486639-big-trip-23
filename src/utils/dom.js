c45ff6b3570a298db3d131958805a454cca99e3a
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-05-14 18:01:11 +0700

    докоммит

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,6 +1,7 @@
 const createElementsTemplate =
   (elements, createElementTemplate, ...rest) => (elements) ? elements.map((element) => createElementTemplate(element, ...rest)).join(' ') : '';
 
+//! название подобрать
 const createMapElementsTemplate =
   (mapElements, createElementTemplate, ...rest) => {
     const strings = [];
