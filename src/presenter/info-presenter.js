c327f9202ed3927d3bad25b85ecca57a0124853e
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-05-15 22:19:28 +0700

    ошибка

--- src/presenter/info-presenter.js
+++ src/presenter/info-presenter.js
@@ -16,7 +16,7 @@ export default class InfoPresenter {
 
   init() {
     //! временно, разный текст в зависмости от фильтра, и обработка нажатий
-    if (this.#events.length) {
+    if (this.#events.size) {
       const { offers, destinations } = this.#eventsModel;
       const destinationData = Array.from(destinations, ([, { name }]) => ({ name }));
       render(new InfoView(this.#events, destinationData, offers), this.#containerElement, RenderPosition.AFTERBEGIN);
