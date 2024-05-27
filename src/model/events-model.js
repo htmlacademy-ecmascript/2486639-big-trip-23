949f96026d3ef29182dd25a1fd30cd17cf50af95
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-05-27 09:31:11 +0700

    рефакториг

--- src/model/events-model.js
+++ src/model/events-model.js
@@ -1,8 +1,9 @@
 import { generateMockData } from '../mock/events.js';
 import { EVENT_TYPES } from '../const.js';
+import { makeExtendedEvent } from '../utils/event.js';
 
 export default class EventsModel {
-  #destinations = new Map();
+  #destinationsByName = new Map();
   #offers = new Map();
   #events = [];
 
@@ -10,8 +11,13 @@ export default class EventsModel {
     //! временно - скорее всего нужно сделать init()
     const { destinations, offers, events } = generateMockData(EVENT_TYPES);
 
+    // подготовим дополнительный справочкик для конвертации событий
+    //! возможно destinationsById придеться сохранить в this, т.к. после изменения или добавления сервере придеться перечитывать или оставить, что есть
+    const destinationsById = new Map();
+
     destinations.forEach((destination) => {
-      this.destinations.set(destination.name, destination);
+      this.#destinationsByName.set(destination.name, destination);
+      destinationsById.set(destination.id, destination);
     });
 
     // перекладываем в Map для удобства поиска по type
@@ -20,26 +26,13 @@ export default class EventsModel {
       this.#offers.set(type, typeOffers);
     });
 
-    // подготовим дополнительный справочкик для конвертации событий
-    const destinationsMap = new Map();
-    destinations.forEach((destination) => {
-      destinationsMap.set(destination.id, destination);
-    });
-
     // в событиях переконвертируем офферы в Set и добавим информацию по точке назначения и офферам
     // events осталяем массивом т.к. есть сортировка и фильтрация
-    this.#events = events.map((event) => {
-      const { type, destination, offers: offerIds } = event;
-      const offersMap = new Set(offerIds);
-      const destinationInfo = destinationsMap.get(destination);
-      const typeOffers = this.#offers.get(type);
-
-      return { ...event, offers: offersMap, destinationInfo, typeOffers };
-    });
+    this.#events = events.map((event) => makeExtendedEvent(event, this.#offers, destinationsById));
   }
 
   get destinations() {
-    return this.#destinations;
+    return this.#destinationsByName;
   }
 
   get offers() {
--- src/utils/event.js
+++ src/utils/event.js
@@ -1,4 +1,13 @@
+const makeExtendedEvent = (event, offers, destinations) => {
+  const { type, destination, offers: offerIds } = event;
+  const offersMap = new Set(offerIds);
+  const destinationInfo = destinations.get(destination);
+  const typeOffers = offers.get(type);
+
+  return { ...event, offers: offersMap, destinationInfo, typeOffers };
+};
+
 const getEventOffers = (typeOffers, eventOfferIds) => typeOffers.filter((typeOffer) => eventOfferIds.has(typeOffer.id));
 const getDestinationName = (destinationInfo) => (destinationInfo) ? destinationInfo.name : '';
 
-export { getEventOffers, getDestinationName };
+export { makeExtendedEvent, getEventOffers, getDestinationName };
