7f4338f11b10d8d0ab69c6626699100fdfb5154a
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-05-30 10:54:19 +0700

    откат / Delete utils directory

--- utils/string.js
+++
@@ -1,5 +0,0 @@
-const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
-
-const getTwoDigitString = (number) => (number < 10) ? `0${number}` : number.toString();
-
-export { capitalizeFirstLetter, getTwoDigitString };
